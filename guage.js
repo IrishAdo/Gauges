696c419cd70ba26bfe49ebdcd6a3a89fcba0950a
Adrian Sweeney <adrian@tomorrow-people.com>
2015-07-07 12:09:45 +0100

    Moved all canvas functions into a Factory section.
    This allows the future addition of a SVG Factory to render SVG files if needed

--- guage.js
+++ guage.js
@@ -1,13 +1,22 @@
 var Guage = {
 	list: [],
 	idCounter:-1,
+	factory:null,
 	//	since we are not doing a 360 degree graph we will use 270
 	singleDegree: 270 / 100,
 	// register a canvas element as a Guage
 	register: function(obj) {
+		var isIE = false;
 		var el = obj;// document.getElementById(id);
 		var id = 'Guage'+Guage.idCounter++;
-		el.innerHTML ="<canvas id='"+id+"' width='"+el.getAttribute('width')+"' height='"+el.getAttribute('height')+"'></canvas>";
+		if(isIE){
+			Guage.factoryObject = Guage.Factory.SVG;
+			el.innerHTML ="<svg id='"+id+"' width='"+el.getAttribute('width')+"' height='"+el.getAttribute('height')+"'></svg>";
+		} else {
+			Guage.factoryObject = Guage.Factory.Canvas;
+			el.innerHTML ="<canvas id='"+id+"' width='"+el.getAttribute('width')+"' height='"+el.getAttribute('height')+"'></canvas>";
+		}
+
 		Guage.list[id] = {
 			id: id,
 			canvas: document.getElementById(id),
@@ -50,101 +59,6 @@ var Guage = {
 			Guage.Draw(id);
 		}
 	},
-	Draw: function(id) {
-		var canvas = Guage.list[id].canvas;
-		if (canvas.getContext) {
-			var ctx = canvas.getContext('2d');
-			ctx.fillStyle = "#ffffff";
-			ctx.translate(0, 0);
-			ctx.fillRect(0, 0, Guage.list[id].width, Guage.list[id].height);
-			ctx.save();
-
-			ctx.lineWidth = 49;
-			for (var index = 1; index < Guage.list[id].set.length; index++) {
-				Guage.DrawSegment(ctx, id, index);
-			}
-			Guage.OutlineGuage(ctx);
-			Guage.DrawNeedle(ctx, (45 + (Guage.singleDegree * Guage.list[id].valuePercent)));
-			Guage.DrawLabelsAndTicks(ctx, id);
-		}
-
-	},
-	DrawSegment: function(ctx, id, index) {
-		var path = new Path2D();
-		ctx.strokeStyle = Guage.list[id].colors[index - 1];
-		var startDegrees = (135 + (Guage.singleDegree * Guage.list[id].set[index -
-			1]));
-		var endDegrees = (135 + (Guage.singleDegree * Guage.list[id].set[index]) +
-			0.5);
-		path.arc(125, 125, 75, Guage.DegreeToRadians(startDegrees), Guage.DegreeToRadians(
-			endDegrees), false); // Outer circle
-		ctx.stroke(path);
-
-	},
-	DrawLabelsAndTicks: function(ctx, id) {
-		var maxIndex = Guage.list[id].labels.length;
-		var stepPercentage = 100 / (maxIndex - 1);
-		ctx.strokeStyle = "#000000";
-		ctx.fillStyle = "#000000";
-		ctx.lineWidth = 1;
-		
-		for (var index = 0; index < maxIndex; index++) {
-			ctx.save();
-			var angle = Guage.GetDegrees(45+ (Guage.singleDegree * (stepPercentage * index)))
-			var dx = (Math.cos(Guage.DegreeToRadians(angle+90))*120);
-			var dy = (Math.sin(Guage.DegreeToRadians(angle+90))*120);
-			
-			ctx.fillText(Guage.list[id].labels[index], 125+dx-(10-index),125+dy+(10-index));
-		
-			// draw tick
-			var path = new Path2D();
-			path.moveTo(0, 90);
-			path.lineTo(0, 100);
-			ctx.translate(125, 125);
-			ctx.rotate(Guage.DegreeToRadians(angle));
-			ctx.stroke(path);
-			ctx.restore();
-
-			// ctx.save();
-			// ctx.font = "12px serif";
-			// ctx.translate(125, 125);
-			// ctx.rotate(Guage.DegreeToRadians(45 + (Guage.singleDegree * stepPercentage *
-			// 	index)));
-			// ctx.fillText(Guage.list[id].labels[index], 0, 115);
-			// ctx.restore();
-		}
-	},
-	OutlineGuage: function(ctx) {
-		var path = new Path2D();
-		ctx.lineWidth = 1.5;
-		ctx.strokeStyle = "rgba(0,0,0,1)";
-		path.arc(125, 125, 100, Guage.DegreeToRadians(135), Guage.DegreeToRadians(45), false); // Outer circle
-		path.arc(125, 125, 50, Guage.DegreeToRadians(45), Guage.DegreeToRadians(135),true); // Outer circle
-		path.arc(125, 125, 100, Guage.DegreeToRadians(135), Guage.DegreeToRadians(135), true); // Outer circle
-		ctx.stroke(path);
-	},
-	DrawNeedle: function(ctx, degrees) {
-		ctx.save();
-		ctx.strokeStyle = "rgba(0,0,0,1)";
-		var path = new Path2D();
-		path.arc(125, 125, 3, 0, Math.PI * 2, true);
-		ctx.lineWidth = 3;
-		ctx.stroke(path);
-		ctx.restore();
-
-		ctx.strokeStyle = "#000000";
-		ctx.fillStyle = "#000000";
-		ctx.lineWidth = 1;
-		var path = new Path2D();
-		path.moveTo(0, 80);
-		path.lineTo(-4, 0);
-		path.lineTo(4, 0);
-		ctx.translate(125, 125);
-		ctx.rotate((Math.PI / 180) * degrees);
-		ctx.fill(path);
-		ctx.restore();
-	},
-
 	DegreeToRadians: function(degrees) {
 		if (degrees > 360) {
 			degrees = Guage.GetDegrees(degrees - 360);
@@ -159,6 +73,118 @@ var Guage = {
 			return Guage.GetDegrees(degrees - mod,mod);
 		}
 		return degrees;
-	}
+	},
+	Draw: function(id) {
+		Guage.factoryObject.Draw(id);
+	},
+	DrawSegment: function(ctx, id, index) {
+		Guage.factoryObject.DrawSegment(ctx, id, index);
+	},
+	DrawLabelsAndTicks: function(ctx, id) {
+		Guage.factoryObject.DrawLabelsAndTicks(ctx, id);
+	},
+	OutlineGuage: function(ctx) {
+		Guage.factoryObject.OutlineGuage(ctx);
+	},
+	DrawNeedle: function(ctx, degrees) {
+		Guage.factoryObject.DrawNeedle(ctx, degrees);
+	},
+	Factory:{
+		Canvas :{
+			Draw: function(id) {
+				var canvas = Guage.list[id].canvas;
+				if (canvas.getContext) {
+					var ctx = canvas.getContext('2d');
+					ctx.fillStyle = "#ffffff";
+					ctx.translate(0, 0);
+					ctx.fillRect(0, 0, Guage.list[id].width, Guage.list[id].height);
+					ctx.save();
 
+					ctx.lineWidth = 49;
+					for (var index = 1; index < Guage.list[id].set.length; index++) {
+						Guage.DrawSegment(ctx, id, index);
+					}
+					Guage.OutlineGuage(ctx);
+					Guage.DrawNeedle(ctx, (45 + (Guage.singleDegree * Guage.list[id].valuePercent)));
+					Guage.DrawLabelsAndTicks(ctx, id);
+				}
+
+			},
+			DrawSegment: function(ctx, id, index) {
+				var path = new Path2D();
+				ctx.strokeStyle = Guage.list[id].colors[index - 1];
+				var startDegrees = (135 + (Guage.singleDegree * Guage.list[id].set[index -
+					1]));
+				var endDegrees = (135 + (Guage.singleDegree * Guage.list[id].set[index]) +
+					0.5);
+				path.arc(125, 125, 75, Guage.DegreeToRadians(startDegrees), Guage.DegreeToRadians(
+					endDegrees), false); // Outer circle
+				ctx.stroke(path);
+
+			},
+			DrawLabelsAndTicks: function(ctx, id) {
+				var maxIndex = Guage.list[id].labels.length;
+				var stepPercentage = 100 / (maxIndex - 1);
+				ctx.strokeStyle = "#000000";
+				ctx.fillStyle = "#000000";
+				ctx.lineWidth = 1;
+				
+				for (var index = 0; index < maxIndex; index++) {
+					ctx.save();
+					var angle = Guage.GetDegrees(45+ (Guage.singleDegree * (stepPercentage * index)))
+					var dx = (Math.cos(Guage.DegreeToRadians(angle+90))*120);
+					var dy = (Math.sin(Guage.DegreeToRadians(angle+90))*120);
+					
+					ctx.fillText(Guage.list[id].labels[index], 125+dx-(10-index),125+dy+(10-index));
+				
+					// draw tick
+					var path = new Path2D();
+					path.moveTo(0, 90);
+					path.lineTo(0, 100);
+					ctx.translate(125, 125);
+					ctx.rotate(Guage.DegreeToRadians(angle));
+					ctx.stroke(path);
+					ctx.restore();
+
+					// ctx.save();
+					// ctx.font = "12px serif";
+					// ctx.translate(125, 125);
+					// ctx.rotate(Guage.DegreeToRadians(45 + (Guage.singleDegree * stepPercentage *
+					// 	index)));
+					// ctx.fillText(Guage.list[id].labels[index], 0, 115);
+					// ctx.restore();
+				}
+			},
+			OutlineGuage: function(ctx) {
+				var path = new Path2D();
+				ctx.lineWidth = 1.5;
+				ctx.strokeStyle = "rgba(0,0,0,1)";
+				path.arc(125, 125, 100, Guage.DegreeToRadians(135), Guage.DegreeToRadians(45), false); // Outer circle
+				path.arc(125, 125, 50, Guage.DegreeToRadians(45), Guage.DegreeToRadians(135),true); // Outer circle
+				path.arc(125, 125, 100, Guage.DegreeToRadians(135), Guage.DegreeToRadians(135), true); // Outer circle
+				ctx.stroke(path);
+			},
+			DrawNeedle: function(ctx, degrees) {
+				ctx.save();
+				ctx.strokeStyle = "rgba(0,0,0,1)";
+				var path = new Path2D();
+				path.arc(125, 125, 3, 0, Math.PI * 2, true);
+				ctx.lineWidth = 3;
+				ctx.stroke(path);
+				ctx.restore();
+
+				ctx.strokeStyle = "#000000";
+				ctx.fillStyle = "#000000";
+				ctx.lineWidth = 1;
+				var path = new Path2D();
+				path.moveTo(0, 80);
+				path.lineTo(-4, 0);
+				path.lineTo(4, 0);
+				ctx.translate(125, 125);
+				ctx.rotate((Math.PI / 180) * degrees);
+				ctx.fill(path);
+				ctx.restore();
+			},
+		}
+	}
 }
